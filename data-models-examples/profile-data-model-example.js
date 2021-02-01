4c594f58366750ff3039bff0f192e4d381de604c
mydatahack <thon6thon6@hotmail.com>
2021-02-01 14:43:43 +1100

    Add Data model example - profile

---
+++ data-models-examples/profile-data-model-example.js
@@ -0,0 +1,121 @@
+[{
+  ProfilePK: 'Account#5001',
+  AccountId: 5001,
+  Regions: [
+    {
+      Region: 'Melbourne',
+      State: 'VIC'
+    },
+    {
+      Region: 'Geelong',
+      State: 'VIC'
+    }
+  ],
+  Trades: [
+    {
+      TradeId: 111,
+      TradeName: 'Asbestos'
+    },
+    {
+      TradeId: 112,
+      TradeName: 'Windows'
+    },
+    {
+      TradeId: 113,
+      TradeName: 'Stones'
+    },
+  ],
+  Cerficates: [
+    {
+      fileName: 'my-cert-stone-work.pdf',
+      fileSize: '10kb',
+      fmHash: 'sadfasdf34534534sdf',
+      DocumentName: 'Certificate of being a stoner',
+      ExpiryDate: '2022-01-01'
+
+    },
+    {
+      fileName: 'my-cert-stone-work.pdf',
+      fileSize: '10kb',
+      fmHash: 'sadfasdf34534534sdf',
+      DocumentName: 'Certificate of being a stoner',
+      ExpiryDate: '2022-01-01'
+
+    }
+  ],
+  Contact: [
+    {
+      UserId: 111,
+      UserName: 'Ken Best',
+      OfficeId: 111,
+      Email: 'ken.best@stonework.com',
+      Phone: '0400000000',
+      Position: 'Brick Master'
+    },
+  ],
+  Avatar: {
+    fileName: 'stoneworks-logo.jpeg',
+    fileSize: '10kb',
+    fmHash: 'sadfasdf34534534sdf',
+  },
+  CompanyInfo: 'Hey we are awesome',
+  Budget: '$2 to $2 billions!',
+  WorkforceSize: '200'
+},
+{
+  ProfilePK: 'Account#5001',
+  ProfileSK: 'Project#4001',
+  AccountId: 5001,
+  ProjectId: 4001,
+  Photos: [
+    {
+      FileName: 'project-4001-outside.jpeg',
+      FileSize: '200kb',
+      FileHash: 'sdf234dsdfad56sdf',
+      Description: 'This is outside.',
+      UploadedTimestamp: '2020-01-01T12:00:00.000',
+      CoverPhoto: true
+    },
+    {
+      FileName: 'project-4001-back.jpeg',
+      FileSize: '111kb',
+      FileHash: 'sdf23dsdfgdh456sdf',
+      Description: 'This is back.',
+      UploadedTimestamp: '2020-01-01T14:00:00.000',
+      CoverPhoto: false
+    },
+    {
+      FileName: 'project-4001-yard.jpeg',
+      FileSize: '122kb',
+      FileHash: 'sdf2sdfasdfasd56sdf',
+      Description: 'This is yard.',
+      UploadedTimestamp: '2020-01-01T15:00:00.000',
+      CoverPhoto: false
+    },
+  ],
+  Endorsement: {
+    BuilderId: 4001,
+    Trade: ['Asbestos', 'Window', 'Stone work'],
+    Remark: 'He rocks!',
+    Approved: true
+  },
+  Name: 'Merri Creak Stadium Project',
+  Budget: '5 - 10 millions',
+  ConstructionType: 'Commercial',
+  Sectors: 'Entertainment',
+  Status: 'Completed',
+  BuilderInfo: {
+    Name: 'Tom Hart'
+  },
+  Summary: 'We build sick rock stadium!',
+  StartDate: '2019-01-01',
+  CompletionDate: '2020-12-25', 
+},
+{
+  ProfilePK: 'Account#5001',
+  ProfileSK: 'Project#4002',
+  AccountId: 5001,
+  ProjectId: 4002,
+},
+
+];
---
+++ data-models-examples/profile-data-model.md
@@ -0,0 +1,6 @@
+# Data Model with DynamoDB Example - Profile Data
+
+## Strategy
+
+To model data with DynamoDB, we can start with ERD, access pattern and partition & sort keys.
+
