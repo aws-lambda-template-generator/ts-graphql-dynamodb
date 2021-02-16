7547a0217a53768627500dd679653725d2a5dc26
mydatahack <thon6thon6@hotmail.com>
2021-02-16 14:46:54 +1100

    Add example data

--- data-models-examples/movies-data-example.js
+++ data-models-examples/movies-data-example.js
@@ -1,10 +1,10 @@
 [
   {
-    MoviesPK: 'Movie#1',
-    Id: 1,
+    Year: '1994',
     Title: 'Interview with the Vampire',
     Country: 'United States',
     Director: 'Neil Jordan',
+    Genra: ['Vampire', 'Drama'],
     Cast: [
       {
         Character: 'Louis de Pointe du Lac',
@@ -28,34 +28,295 @@
     Quotes: [
       {
         quote: [
-          'Lestat: \'Evil is a point of view. God kills indiscriminately and so shall we. For no creatures under God are as we are, none so like him as ourselves.\''
+          'Lestat: Evil is a point of view. God kills indiscriminately and so shall we. For no creatures under God are as we are, none so like him as ourselves.'
         ]
       },
       {
         quote: [
-          'Louis: \'The statue seemed to move, but didn\'t. The world had changed, yet stayed the same. I was a newborn vampire weeping at the beauty of the night.\''
+          'Louis: The statue seemed to move, but didn\'t. The world had changed, yet stayed the same. I was a newborn vampire weeping at the beauty of the night.'
         ]
       },
       {
         quote: [
-          'Malloy: \'So there are no vampires in Trasylvania? No Count Dracula?\'',
-          'Louis: \'Fictions, my friend. The vulgar fictions of a demented Irishman.\''
+          'Malloy: So there are no vampires in Trasylvania? No Count Dracula?',
+          'Louis: Fictions, my friend. The vulgar fictions of a demented Irishman.'
         ]
       },
       {
         quotes: [
-          'Malloy: \'What about crucifixes?\'',
-          'Louis: \'Crucifixes?\'',
-          'Malloy: \'Yes, can you look at them?\'',
-          'Louis: \'Actually I am quite fond of looking at crucifixes.\'',
-          'Molloy: \'What about the old stake through the heart?\'',
-          'Louis: \'Nonesense.\'',
-          'Molloy: \'Coffins? What about coffins?\'',
-          'Louis: \'Coffins. Coffins, I\'m afraid, are a necessity\''
+          'Malloy: What about crucifixes?',
+          'Louis: Crucifixes?',
+          'Malloy: Yes, can you look at them?',
+          'Louis: Actually I am quite fond of looking at crucifixes.',
+          'Molloy: What about the old stake through the heart?',
+          'Louis: Nonesense.',
+          'Molloy: Coffins? What about coffins?',
+          'Louis: Coffins. Coffins, I\'m afraid, are a necessity'
         ]
       }
     ],
-    Year: '1994',
     Language: 'English'
-  }
+  },
+  {
+    Year: '1882',
+    Title: 'Blade Runner',
+    Country: 'United States',
+    Director: 'Ridley Scott',
+    Genra: ['Science Fiction', 'Cyberpunk'],
+    Cast: [
+      {
+        Character: 'Rick Deckard',
+        Actor: 'Harrison Ford'
+      },
+      {
+        Character: 'Roy Batty',
+        Actor: 'Rutger Hauer'
+      },
+      {
+        Character: 'Racheal',
+        Actor: 'Sean Yound'
+      },
+      {
+        Character: 'Detective Gaff',
+        Actor: 'Edward James Olmos'
+      }
+    ],
+    Description: 'Science fiction film set in a dystopian future LA of 2019. It is a cult classic.',
+    whyShouldWeWatch: 'Cyberpunk classic. The quality of grainy shots on the films really suits the dystopian world and story line. Great adoptation of Phillp K. Dick\'s SF classic',
+    Quotes: [
+      {
+        quote: [
+          'Roy Batty: I\'ve done questionable things.',
+          'Tyrell: Also extraordinary things. Revel in your time.',
+          'Roy Batty: Nothing the God of bio-mechanics wouldn\'t let you in heaven for.'
+        ]
+      },
+      {
+        quote: [
+          'Rachael: Like our owl?',
+          'Rick Deckard: It\'s artificial.',
+          'Rachael: Of course it is.',
+          'Rick Deckard: Must be expensive.',
+          'Rachael: Very. I\'m Rachael.',
+          'Rick Deckard: Deckard.'
+        ]
+      },
+      {
+        quote: [
+          'Tyrell: More human than human is our motto.'
+        ]
+      },
+      {
+        quotes: [
+          'Roy Batty: I\'ve seen things you people wouldn\'t believe. Attack ships on fire off the shoulder of Orion. I\'ve watched C-beams glitter in the dark near the Tannhauser Gate. All those moments will be lost in time, like tears in the rain. Time to die.'
+        ]
+      }
+    ],
+    Language: 'English'
+  },
+  {
+    Year: '1882',
+    Title: 'E.T. the Extra-Terrestrial',
+    Country: 'United States',
+    Director: 'Steven Spielberg',
+    Genra: ['Science Fiction', 'Drama'],
+    Cast: [
+      {
+        Character: 'Elliott',
+        Actor: 'Henry Thomas'
+      },
+      {
+        Character: 'Mary',
+        Actor: 'Dee Wallace'
+      },
+      {
+        Character: 'Keys',
+        Actor: 'Peter Coyote'
+      },
+      {
+        Character: 'Michael',
+        Actor: 'Robert MacNaughton'
+      },
+      {
+        Character: 'Gertie',
+        Actor: 'Drew Barrymore'
+      }
+    ],
+    Description: 'Science fiction film about a story of Elliott becoming a friend with an extraterrestrial, dubbed E.T. who is stranded on Earth',
+    whyShouldWeWatch: 'This block buster film made E.T. to enter into our common English vocaburary. Classic Spielberg coming of age story about friendship with a Si-Fi twist. Of course, who can pass the iconic finger tip alian greetings.',
+    Quotes: [
+      {
+        quote: [
+          'Gertie: I don\'t like his feet.',
+          'Elliott: They\'re only feet, you little twerp.'
+        ]
+      },
+      {
+        quote: [
+          'E.T.: Phone. Home. E.T. home phone'
+        ]
+      },
+      {
+        quote: [
+          'Scientist: You said it has the ability to manipulate its own environment?',
+          'Michael: He\'s smart. He communicates through Elliott.',
+          'Scientist: Elliott thinks its thoughts.',
+          'Michael: No, Elliott... Elliott feels his feelings.'
+        ]
+      },
+      {
+        quotes: [
+          'Eliott: 35 pounds? You\'re fat!'
+        ]
+      }
+    ],
+    Language: 'English'
+  },
+  {
+    Year: '1884',
+    Title: 'Scarface',
+    Country: 'United States',
+    Director: 'Brian De Palma',
+    Genra: ['Crime Fiction', 'Gangster Film'],
+    Cast: [
+      {
+        Character: 'Tony Montana',
+        Actor: 'Al Pacino'
+      },
+      {
+        Character: 'Manny Ray',
+        Actor: 'Steven Bauer'
+      },
+      {
+        Character: 'Elvira',
+        Actor: 'Michelle Pfeiffer'
+      },
+      {
+        Character: 'Gina',
+        Actor: 'Mary Elizabeth Mastrantonio'
+      },
+      {
+        Character: 'Frank Lopez',
+        Actor: 'Robert Loggia'
+      }
+    ],
+    Description: 'A rags-to-riches story about Tony Montana who arrives penniless in Miami and become a powerful drug load.',
+    whyShouldWeWatch: 'Classic rags-to-riches story with a big come down ending. Tony Montana is one of the most iconic characters in the cinema history. Music by Giorgio Moroder is absolutly supurb.',
+    Quotes: [
+      {
+        quote: [
+          'Frank Lopez: Lesson no.1. Never understimate the other guy\'s greed. Lesson no.2. Never get high on your own supply',
+        ]
+      },
+      {
+        quote: [
+          'Tony Montana: I\'m Tony Montana, you fuck with me! You fuckin\' with the best!'
+        ]
+      },
+      {
+        quote: [
+          'Tony Montana: Why don\'t you try sticking your head up your ass, see if it fits.'
+        ]
+      },
+      {
+        quotes: [
+          'Tony Montana: I always tell the truth even when I lie.'
+        ]
+      },
+      {
+        quotes: [
+          'Tony Montana: So say goodnight to the bad guy!'
+        ]
+      },
+      {
+        quotes: [
+          'Alejandro Sosa: I told you a long time ago, you fucking little monkey, not to fuck me!'
+        ]
+      },
+      {
+        quotes: [
+          'Tony Montana: You think I\'ll kill two kids and a woman? Fuck that! I don\'t need that kind of shit in my life!'
+        ]
+      }
+    ],
+    Language: 'English'
+  },
+  {
+    Year: '1887',
+    Title: 'Full Metal Jacket',
+    Country: 'United States',
+    Director: 'Stanley Kubrick',
+    Genra: ['War'],
+    Cast: [
+      {
+        Character: 'Private/Sergeant J. T. "Joker" Davis',
+        Actor: 'Matthew Modine'
+      },
+      {
+        Character: 'Private Leonard "Gomer Pyle" Lawrence',
+        Actor: 'Vincent D\'Onofrio'
+      },
+      {
+        Character: 'Gunnery Sergeant Hartman',
+        Actor: 'Lee Ermey'
+      },
+      {
+        Character: 'Private/Sergeant "Cowboy" Evans',
+        Actor: 'Arliss Howard'
+      },
+      {
+        Character: 'Animal Mother',
+        Actor: 'Adam Baldwin'
+      }
+    ],
+    Description: 'A classic Vietnam war film by Kubric',
+    whyShouldWeWatch: 'The intensity of the subject matter, acting and storyline makes this film a must-see.',
+    Quotes: [
+      {
+        quote: [
+          'Soldiers: This is my rifle. There are many others like it, but this one is mine. My rifle is my best friend. It is my life. I must master it as I must master my life. Without me, my rifle is useless. Without my rifle, I am useless. I must fire my rifle true. I must shoot straighter than my enemy, who is trying to kill me. I must shoot him before he shoots me. I will. Before God I swear this creed: my rifle and myself are defenders of my country, we are the masters of our enemy, we are the saviors of my life. So be it, until there is no enemy, but peace. Amen.',
+        ]
+      },
+      {
+        quote: [
+          'Gunnery Sgt. Hartman: I am Gunnery Sgt. Hartman, your senior drill instructor. From now on you will speak only when spoken to, and the first and last words out of your filthy sewers will be \'Sir.\' Do you maggots understand that?'
+        ]
+      },
+      {
+        quote: [
+          'Pvt. Pyle: Seven-six-two-millimeter, full metal jacket.'
+        ]
+      },
+      {
+        quotes: [
+          'Gunnery Sgt. Hartman: There is no racial bigatory here. I don\'t look down on nigger, kikes, wogs or greasers. Here, you\'re all equally worthless.'
+        ]
+      },
+      {
+        quotes: [
+          'Gunnery Sgt. Hartman: Do you maggots understand that?',
+          'Soldiers: Sir, yes, sir',
+          'Gunnery Sgt. Hartman: Bullshit, I can\'t hear you.'
+        ]
+      },
+      {
+        quotes: [
+          'Alejandro Sosa: I told you a long time ago, you fucking little monkey, not to fuck me!'
+        ]
+      },
+      {
+        quotes: [
+          'Gunnery Sgt. Hartman: Holy Jesus! What is that? What the fuck is that?! What is that, Pvt. Pyle?',
+          'Pvt. Pyle: Sir, a jelly doughnut, sir!',
+          'Gunnery Sgt. Hartman: A Jelly doughnut?'
+        ]
+      },
+      {
+        quotes: [
+          'Da Nang Hooker: Well, baby, me so horny. Me so HORNY. Me love you long time. You party?'
+        ]
+      },
+    ],
+    Language: 'English'
+  },
 ];
