8097ffe6b8a1799bb042717b38f611ee495e9a1d
mydatahack <thon6thon6@hotmail.com>
2021-02-17 12:43:05 +1100

    Troubleshooting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,66 @@
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true
+  root: true,
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:fp/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  env: {
+    node: true,
+    jest: true,
   },
-  'parser': '@typescript-eslint/parser',
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module'
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+    'fp',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+          extensions: ['.ts'],
+          paths: ['node_modules/', 'node_modules/@types']
+      },
+      typescript: {},
+    },
   },
-  'rules': {
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: 2019,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': ['error', 'never'],
+    'import/prefer-default-export': 'off',
+    'radix': 'off',
     semi: ['error', 'always'],
     quotes: [2, 'single'],
     indent: ['error', 2]
-  }
+  },
+  overrides: [
+    // Models use classes and decorators
+    {
+      files: ['src/entities/*.ts'],
+      rules: {
+        'fp/no-class': 'off',
+        'fp/no-this': 'off',
+        'fp/no-mutation': 'off',
+        'import/no-cycle': 'off',
+      },
+    },
+    {
+      files: ['src/services/SessionService.ts'],
+      rules: {
+        'fp/no-mutation': ['off'],
+        'fp/no-nil': ['off'],
+      },
+    },
+  ]
 };
